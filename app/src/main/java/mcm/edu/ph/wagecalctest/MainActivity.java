1ceabdc291c2d0a5d7cc2f9f760362eff86c9b13
adibrv <adibruv@gmail.com>
2021-02-01 02:40:46 +0800

    mafumafu

--- app/src/main/java/mcm/edu/ph/wagecalctest/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/wagecalctest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button computePay;
@@ -25,10 +28,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick (View v) {
 
-        EditText workTimeInput;
+        EditText workTimeInput, regPayTxt, ovPayTxt;
         TextView totalPay;
 
         workTimeInput = findViewById(R.id.workTimeInput);
+        regPayTxt = findViewById(R.id.regPayTxt);
+        ovPayTxt = findViewById(R.id.ovPayTxt);
         totalPay = findViewById(R.id.totalPayText);
 
         double workingHours = 0.0;
@@ -39,32 +44,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         double max = 8;
 
 
-        double regularPayFormula, overtimePayFormula1, overtimePayFormula2;
+        double regularPayFormula, overtimePayFormula1, overtimePayFormula2, maxRegularPay;
 
         workingHours = Double.parseDouble(workTimeInput.getText().toString());
-        regularPay = 100;
-        overtimePay = 130;
+        regularPay = Double.parseDouble(regPayTxt.getText().toString());
+        overtimePay = Double.parseDouble(ovPayTxt.getText().toString());
 
         regularPayFormula = workingHours * regularPay;
         overtimePayFormula1 = workingHours - max;
         overtimePayFormula2 = overtimePayFormula1 * overtimePay;
+        maxRegularPay = regularPay * max;
 
         if (workingHours < 8) {
             totalPayout = regularPayFormula;
         }
 
-        if (workingHours == 8) {
-            totalPayout = 800;
+        if (workingHours == 8 ) {
+            totalPayout = maxRegularPay;
         }
 
         if (workingHours > 8) {
-            totalPayout = 800 + overtimePayFormula2;
+            totalPayout = overtimePayFormula2 + maxRegularPay;
         }
 
         switch (v.getId()) {
             case R.id.computePay:
                 answer = totalPayout;
-                totalPay.setText(Double.toString(answer));
+                totalPay.setText("P"+Double.toString(answer)+ "" );
                 break;
         }
     }
